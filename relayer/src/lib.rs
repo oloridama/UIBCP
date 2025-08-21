7f24d1b76f38ef28ea71a7c562842119c6225538
oloridama <105488299+oloridama@users.noreply.github.com>
2025-08-21 07:26:17 +0100

    Update lib.rs

--- relayer/src/lib.rs
+++ relayer/src/lib.rs
@@ -11,12 +11,8 @@ use winter_air::AirContext;
 // Include generated Protobuf structs
 include!(concat!(env!("OUT_DIR"), "/uibc_gen.rs"));
 
-pub mod relayer {
-    pub mod src {
-        pub mod zk {
-            pub mod circuit;
-        }
-    }
+pub mod zk {
+    pub mod circuit;
 }
 
 // Relayer Logic
